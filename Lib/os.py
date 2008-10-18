int3/jython

commit 660c53788780f76281d3efd22ce5b14df0355eb9

fix os.access not handling multiple modes, and make its X_OK work via jna-posix's stat
git-svn-id: https://jython.svn.sourceforge.net/svnroot/jython/trunk/jython@5420 79b11f02-f210-0410-99aa-fb189b3d1ac4

Lib/os.py
@@ -421,15 +421,17 @@ def access(path, mode):
         raise TypeError('an integer is required')
 
     f = File(sys.getPath(path))
+    result = True
     if not f.exists():
-      return False
+        result = False
     if mode & R_OK and not f.canRead():
-        return False
+        result = False
     if mode & W_OK and not f.canWrite():
-        return False
-    if mode & X_OK:
-        return False
-    return True
+        result = False
+    if mode & X_OK and not (stat(path).st_mode & _stat.S_IEXEC):
+        # NOTE: always False without jna-posix stat
+        result = False
+    return result
 
 def stat(path):
     """stat(path) -> stat result

commit 28f686fa45537a3606e0b63097b742d73c803e20

fix os.access to never raise an exception with a valid mode, which I broke in r5420
--HG--
extra : convert_revision : svn%3A79b11f02-f210-0410-99aa-fb189b3d1ac4/jython/trunk%405462

Lib/os.py
@@ -428,9 +428,12 @@ def access(path, mode):
         result = False
     if mode & W_OK and not f.canWrite():
         result = False
-    if mode & X_OK and not (stat(path).st_mode & _stat.S_IEXEC):
+    if mode & X_OK:
         # NOTE: always False without jna-posix stat
-        result = False
+        try:
+            result = (stat(path).st_mode & _stat.S_IEXEC) != 0
+        except OSError:
+            result = False
     return result
 
 def stat(path):